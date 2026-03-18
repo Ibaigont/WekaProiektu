86f7ab4ede1f119e71f1b98f8c8b049659267a38
ibaigont <ibaigont@gmail.com>
2026-03-18 10:17:03 +0100

    csv eta stwv sortuta

--- src/proiektu/TweetSentiment.java
+++ src/proiektu/TweetSentiment.java
@@ -1,7 +1,49 @@
 package proiektu;
 
+import java.io.File;
+import weka.filters.Filter;
+
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+import weka.core.stemmers.SnowballStemmer;
+import weka.filters.unsupervised.attribute.Remove;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+
 public class TweetSentiment {
 	public static void main(String[] args) {
-		
+		if (args.length!=1) {
+			System.err.println("Erabilera: java -jar TweetSentiment.jar <csvPath>");
+		}
+		String csvPath = args[0];
+		try {
+			CSVLoader loader = new CSVLoader();
+			loader.setSource(new File(csvPath));
+			Instances data = loader.getDataSet();
+
+			Remove removeFilter = new Remove();
+			removeFilter.setAttributeIndices("3,4");
+			removeFilter.setInputFormat(data);
+
+			Instances dataFiltratuta = Filter.useFilter(data, removeFilter);
+
+			dataFiltratuta.setClassIndex(1);
+
+			StringToWordVector STWV = new StringToWordVector();
+			STWV.setInputFormat(dataFiltratuta);
+			STWV.setWordsToKeep(1000);
+			STWV.setLowerCaseTokens(true);
+			STWV.setTFTransform(true);
+			STWV.setIDFTransform(true);
+
+			SnowballStemmer stemmer = new SnowballStemmer();
+			stemmer.setStemmer("english");
+			STWV.setStemmer(stemmer);
+
+			Instances dataBektorea = Filter.useFilter(dataFiltratuta, STWV);
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
